a6501892e3adfb68491c3730cb4c3182191e74fc
CodeMari <amaril@uci.edu>
2017-10-23 11:07:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,23 +14,23 @@ const int httpPort = 8888;
 const char* streamId   = "CS244HW2/Assignment2.php";
 
 //Sensor details
-MAX30105 particleSensor;
+#MAX30105 particleSensor;
 
 
-const byte powerLevel = 0x1F; //6.4mA
+#const byte powerLevel = 0x1F; //6.4mA
 //Pwer level settings: 0x02 = 0.4mA, 0x1F = 6.4mA (default), 0x7F = 25.4mA, 0xFF = 50mA
-const byte powerLevel1 = 0x02; //0.4mA 
-const byte powerLevel2 = 0x1F; //6.4mA
-const byte powerLevel3 = 0x7F; //25.4mA
-const byte powerLevel4 = 0xFF; //50mA 
+#const byte powerLevel1 = 0x02; //0.4mA 
+#const byte powerLevel2 = 0x1F; //6.4mA
+#const byte powerLevel3 = 0x7F; //25.4mA
+#const byte powerLevel4 = 0xFF; //50mA 
 
 //No need to change these
 //but coding them in incase we want to mess around
-const int sampleRate = 50;
-const byte sampleAverage = 4;
-const byte ledMode = 3;
-const int pulseWidth = 411;
-const int adcRange = 4096;
+#const int sampleRate = 50;
+#const byte sampleAverage = 4;
+#const byte ledMode = 3;
+#const int pulseWidth = 411;
+#const int adcRange = 4096;
 
 void printMacAddress()
 {
@@ -52,7 +52,7 @@ void printMacAddress()
     Serial.println(WiFi.localIP());
 }
 
-void sendData(int IR, int RED) {
+#void sendData(int IR, int RED) {
 
     //Try connecting to host
     WiFiClient client;
@@ -64,8 +64,8 @@ void sendData(int IR, int RED) {
     //Create the URL
     String url = "/";
     url += streamId;
-    url += "?IR=" + String(IR);
-    url += "&RED=" + String(RED);
+    #url += "?IR=" + String(IR);
+    #url += "&RED=" + String(RED);
 
     //Request URL
     client.print(String("GET ") + url + " HTTP/1.1\r\n" +
@@ -82,15 +82,15 @@ void setupSensorBoard() {
     Serial.println("Initializing sensor board...");
   
     // Initialize sensor
-    while (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+   # while (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
     {
-      Serial.println("MAX30105 was not found. Please check wiring/power. ");
-      delay(500);
+    #  Serial.println("MAX30105 was not found. Please check wiring/power. ");
+     # delay(500);
     }
   
     //Configure sensor.
     //Change the powerLevel each run
-    particleSensor.setup(powerLevel1, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); 
+  # particleSensor.setup(powerLevel1, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); 
 }
 
 void setup()
@@ -104,16 +104,16 @@ void setup()
 
 void loop()
 {
-  int RED = particleSensor.getRed();
-  int IR = particleSensor.getIR();
+ # int RED = particleSensor.getRed();
+ # int IR = particleSensor.getIR();
 
-  Serial.print("IR[");
-  Serial.print(IR);
-  Serial.print("] RED[");
-  Serial.print(RED);
-  Serial.print("]");
-  Serial.println();
+  #Serial.print("IR[");
+  #Serial.print(IR);
+  #Serial.print("] RED[");
+  #Serial.print(RED);
+  #Serial.print("]");
+  #Serial.println();
 
-  sendData(IR,RED);
+  #sendData(IR,RED);
 
 }
