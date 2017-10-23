c4d5944d3651832daccf7d663099f8c9462e6293
CodeMari <amaril@uci.edu>
2017-10-23 11:11:55 -0700

    Update Assignment2.php
    the differences are "#"

--- Assignment2.php
+++ Assignment2.php
@@ -4,13 +4,13 @@ $username = "root";
 $password = "";
 $dbname = "esp8266asgn1";
 
-$IR = $_GET["IR"];
-$RED = $_GET["RED"];
+#$IR = $_GET["IR"];
+#$RED = $_GET["RED"];
 
 if(!file_exists('HW2Vals4.csv')) {
 
 	$file = fopen('HW2Vals4.csv', 'w');
-	fputcsv($file, array('IR', 'RED'));
+	#fputcsv($file, array('IR', 'RED'));
 
 } else {
 
@@ -18,7 +18,7 @@ if(!file_exists('HW2Vals4.csv')) {
 	
 }
 
-$data = array( array( $IR , $RED) );
+#$data = array( array( $IR , $RED) );
 
 foreach($data as $row){
 	fputcsv($file, $row);
